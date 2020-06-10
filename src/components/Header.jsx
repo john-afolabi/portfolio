faf723783a682052c3f5aa871701786bdc1d5eb7
John Afolabi <johnafolabi.da@gmail.com>
2020-06-10 15:13:49 +0100

    Remove header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "@emotion/styled"
-import Logo from "images/ja-logo.png"
+import { useStaticQuery, graphql } from "gatsby"
 
 const HeaderContainer = styled("div")`
   padding-top: 3.75em;
   padding-bottom: 3em;
 `
 
-const HeaderContent = styled("div")`
-  display: flex;
-  justify-content: space-between;
-`
-
-const LogoIcon = styled("img")`
-  max-width: 33px;
-`
-
-const Header = () => (
-  <HeaderContainer>
-    <HeaderContent>
-      <Link to="/">
-        <LogoIcon src={Logo} />
-      </Link>
-    </HeaderContent>
-  </HeaderContainer>
-)
+const Header = () => {
+  return <HeaderContainer></HeaderContainer>
+}
 
 export default Header
